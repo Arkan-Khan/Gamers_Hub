e7b8af4f657ee33adc1953823c330354e82b1fcd
Sahil <sahilshangloo35@gmail.com>
2024-04-27 22:24:47 +0530

    Made Pokemon COD Valorant html pages and made each page responsive.

--- frontend/script.js
+++ frontend/script.js
@@ -1,19 +1,19 @@
 // Inside script.js: Ensure you have no syntax errors and variable names match
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const loginLink = document.getElementById('loginLink'); // Correct variable use
 
-  loginLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      window.location.href = 'login.html'; 
+  loginLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location.href = 'login.html';
   });
 });
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const signUpLink = document.getElementById('signUpLink');
 
-  signUpLink.addEventListener('click', function(event) {
+  signUpLink.addEventListener('click', function (event) {
     event.preventDefault();
     window.location.href = 'signUp.html';
   });
